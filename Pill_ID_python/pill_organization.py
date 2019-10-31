0622e76e9e79ebc327a7650123c884077dc5edeb
Vikram Sharma <vsharma8363@gmail.com>
2019-10-31 18:10:05 -0400

    Jar updating

--- Pill_ID_python/pill_organization.py
+++ Pill_ID_python/pill_organization.py
@@ -5,4 +5,4 @@ pill_img = pill_frame[pill_frame['has_image'] == True]
 xlower = pill_img['medicine_name'].str.lower()
 pill_img['medicine_name']  = xlower
 pill_groupby = pill_img.groupby(by = 'medicine_name')
-print(pill_groupby.to_string())
+print(pill_img.head().to_string())
